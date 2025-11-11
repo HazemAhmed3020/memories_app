5f578ff134c25716d567ab92698f15b5a47a498e
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-11 03:16:35 +0200

    add providers to main function

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        // BlocProvider<AddNoteCubit>(
+        //     create: (context) => AddNoteCubit(),
+        // ),
+        // BlocProvider<NotesCubit>(
+        //   create: (context) => NotesCubit(),
+        // ),
         BlocProvider(create: (context) => NotesCubit()),
         BlocProvider(create: (context) => AddNoteCubit()),
       ],
